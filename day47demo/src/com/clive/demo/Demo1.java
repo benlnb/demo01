f80b5f8b11e691803e90654c99abe9ff90c8775b
RenDong Li <1127576756@qq.com>
2019-12-30 14:48:47 +0800

    第一次提交

---
+++ day47demo/src/com/clive/demo/Demo1.java
@@ -0,0 +1,12 @@
+package com.clive.demo;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		/**
+		 * pwd	查看当前路径
+		 * git init
+		 * git add 文件夹名称	表示把这个文件夹添加到git本地仓库
+		 */
+		System.out.println("main");
+	}
+}
